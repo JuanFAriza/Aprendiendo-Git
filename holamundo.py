1174f1fbb7637b8c6ed84221c785e5dbdd44d0b6
AdminBiocomputo <jf.ariza10@uniandes.edu.co>
2016-10-14 16:31:51 -0500

    Comandos de Git

--- holamundo.py
+++ holamundo.py
@@ -6,3 +6,5 @@ for i in range(10):
 print 'Nueva modificacion'
 
 print 'Numero aleatorio' + format(np.random.random())
+
+print "Comandos Git\n git checkout \n git status \n git add \n git commit -m mensaje \n git push \n git log"
